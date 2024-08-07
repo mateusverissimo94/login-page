8bb20e04df4ad0d5db2658f840cca77f28dbd182
Mateus Barros <mateusverissimo94@gmail.com>
2024-08-07 03:33:33 -0300

    feat: create login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PrimaryInputComponent } from './components/primary-input/primary-input.component';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, DefaultLoginLayoutComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DefaultLoginLayoutComponent,
+    LoginComponent,
+    PrimaryInputComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [provideHttpClient(), provideAnimations()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,50 @@
 import { Component } from '@angular/core';
-import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private router: Router, private loginService: LoginService) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  submit() {
+    this.loginService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe({
+        next: () =>
+          Swal.fire({
+            title: 'Sucesso',
+            text: 'Login feito com sucesso',
+            icon: 'success',
+            confirmButtonText: 'Continuar',
+          }),
+        error: () =>
+          Swal.fire({
+            title: 'Erro inesperado!',
+            text: 'Tente novamente mais tarde',
+            icon: 'error',
+            confirmButtonText: 'Voltar',
+          }),
+      });
+  }
+
+  navigate() {
+    this.router.navigate(['singup']);
+  }
+}
