079f69f8cfc8ddfca8bd468dbfa01cdbf2c2c693
Mateus Barros <mateusverissimo94@gmail.com>
2024-08-09 01:10:13 -0300

    API finalizada

---
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,22 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
